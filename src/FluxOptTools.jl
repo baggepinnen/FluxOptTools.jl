8c70471fbca5698b242dc25db635463f6d4409a6
eskimo-kidnap <56652555+eskimo-kidnap@users.noreply.github.com>
2021-12-13 14:15:11 +0100

    Fix #10 #9
    This commit uses Zygote's built-in copy! functions instead of
    custom copyto!
    
    veclength(grads::Zygote.Grads) was changed to use similar for loop to
    Zygote's implementation of copy!

--- src/FluxOptTools.jl
+++ src/FluxOptTools.jl
@@ -1,83 +1,35 @@
 module FluxOptTools
 
 using LinearAlgebra, Optim, Flux, Zygote, RecipesBase
-import Base.copyto!
 
 export veclength, optfuns
 
-veclength(grads::Zygote.Grads) = sum(typeof(g[1]) !== GlobalRef ? length(g[1]) : 0 for g in grads.grads)
+veclength(grads::Zygote.Grads) = sum(length(grads[p]) for p in grads.params)
 veclength(params::Flux.Params) = sum(length, params.params)
 veclength(x) = length(x)
 Base.zeros(grads::Zygote.Grads) = zeros(veclength(grads))
 Base.zeros(pars::Flux.Params) = zeros(veclength(pars))
 
-# Grads =============================================
-
-function copyto!(v::AbstractArray, grads::Zygote.Grads)
-    @assert length(v) == veclength(grads)
-    s = 1
-    for g in grads.grads
-        if typeof(g[1]) !== GlobalRef
-            l = length(g[2])
-            v[s:s+l-1] .= vec(g[2])
-            s += l
-        end
-    end
-    v
-end
-
-function copyto!(grads::Zygote.Grads, v::AbstractArray)
-    s = 1
-    for g in grads.grads
-        l = length(g[2])
-        g[2] .= reshape(v[s:s+l-1], size(g[2]))
-        s += l
-    end
-    grads
-end
-
-# Params =============================================
-
-function copyto!(v::AbstractArray, pars::Flux.Params)
-    @assert length(v) == veclength(pars)
-    s = 1
-    for g in pars.params
-        l = length(g)
-        v[s:s+l-1] .= vec(g)
-        s += l
-    end
-    v
-end
-
-function copyto!(pars::Flux.Params, v::AbstractArray)
-    s = 1
-    for p in pars.params
-        l = length(p)
-        p .= reshape(v[s:s+l-1], size(p))
-        s += l
-    end
-    pars
-end
 
 
 function optfuns(loss, pars::Union{Flux.Params, Zygote.Params})
     grads = Zygote.gradient(loss, pars)
     p0 = copyto!(zeros(pars), pars)
     gradfun = function (g,w)
-        copyto!(pars, w)
+        copy!(pars, w)
         grads = Zygote.gradient(loss, pars)
-        copyto!(g, grads)
+        copy!(g, grads)
     end
     lossfun = function (w)
-        copyto!(pars, w)
+        copy!(pars, w)
         loss()
     end
     fg! = function (F,G,w)
-        copyto!(pars, w)
+        copy!(pars, w)
         if G != nothing
             l, back = Zygote.pullback(loss, pars)
             grads = back(1)
-            copyto!(G, grads)
+            copy!(G, grads)
             return l
         end
         if F != nothing
@@ -89,7 +41,7 @@ end
 
 @recipe function lossplot(loss::Function, pars::Flux.Params; l=0.1, npoints=30)
     p       = zeros(pars)
-    copyto!(p,pars)
+    copy!(p,pars)
     pcopy   = deepcopy(p)
     n0      = norm(p)
     dx,dy   = randn(length(p)),randn(length(p))
@@ -98,10 +50,10 @@ end
     pertvec = LinRange(-1,1,npoints)
     losses = map(Iterators.product(pertvec,pertvec)) do (lx,ly)
         pi = p + lx*dx + ly*dy
-        copyto!(pars, pi)
+        copy!(pars, pi)
         loss()
     end
-    copyto!(pars, pcopy)
+    copy!(pars, pcopy)
     seriestype --> :contour
     pertvec,pertvec,losses
 end
